dc7b13429c352e22c57763658ca9d5b626af3ca5
Cristina2493 <123487312+Cristina2493@users.noreply.github.com>
2023-03-15 12:31:31 +0100

    ordenar por carpetas

--- src/main/java/com/eoi/springboot/DemoApplication.java
+++ src/main/java/com/eoi/springboot/DemoApplication.java
@@ -1,3 +1,5 @@
+package com.eoi.springboot;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
