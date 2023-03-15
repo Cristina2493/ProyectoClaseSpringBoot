f5b2c263027473ad00ebc79dd6d3cdefbe7c3317
Cristina2493 <123487312+Cristina2493@users.noreply.github.com>
2023-03-15 12:03:58 +0100

    ordenar métodos dentro de una clase

--- src/main/java/DemoApplication.java
+++ src/main/java/DemoApplication.java
@@ -1,6 +1,5 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication //haz tu magia
